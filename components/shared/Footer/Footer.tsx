de96b3e7c6a0e320b40eb73ba45d9ce8fcd5ddf1
Amirali Shirani <amirali7shirani@gmail.com>
2025-08-31 16:57:59 +0330

    changed some part of footer

--- components/shared/Footer/Footer.tsx
+++ components/shared/Footer/Footer.tsx
@@ -2,30 +2,38 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="p-5 gap-8 text-white bg-footer-secondary grid grid-cols-5">
-            <div className="grid-cols-3 flex justify-between">
+        <footer className="p-5  gap-8 text-white bg-footer-secondary ">
+            <div className="container mx-auto grid grid-cols-5 p-5">
+
+            <div className="col-span-3 flex  justify-between  gap-4">
                 <div>
-                    <h2>محبوب ترین ها</h2>
+                    <h3>محبوب ترین ها</h3>
                     <ul>
                         <li>لباس مردانه</li>
                     </ul>
                 </div> <div>
-                    <h2>محبوب ترین ها</h2>
+                    <h3>محبوب ترین ها</h3>
                     <ul>
                         <li>لباس مردانه</li>
                     </ul>
                 </div> <div>
-                    <h2>محبوب ترین ها</h2>
+                    <h3>محبوب ترین ها</h3>
                     <ul>
                         <li>لباس مردانه</li>
                     </ul>
                 </div> <div>
-                    <h2>محبوب ترین ها</h2>
+                    <h3>محبوب ترین ها</h3>
                     <ul>
                         <li>لباس مردانه</li>
                     </ul>
                 </div>
             </div>
+            <div className="col-span-2">
+                <h2>برای دریافت تخفیف های بیشتر ما را دنبال کنید!</h2>
+                <div className=""></div>
+            </div>
+            </div>
+
         </footer>
     );
 };
