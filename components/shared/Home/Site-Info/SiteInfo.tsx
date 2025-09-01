64ab969fd36730120adabe4fa1d6709283a02cb6
Amirali Shirani <amirali7shirani@gmail.com>
2025-09-02 00:01:21 +0330

    added style to site info

--- components/shared/Home/Site-Info/SiteInfo.tsx
+++ components/shared/Home/Site-Info/SiteInfo.tsx
@@ -4,15 +4,15 @@ import InfoCard from "@/components/shared/Home/Site-Info/InfoCard";
 
 const SiteInfo = () => {
     return (
-        <section className="px-52 flex justify-center lg:mt-20 mt-10">
-            <div className=" gap-12 p-4 flex  items-center justify-between">
+        <section className="px-52 py-10 flex justify-center lg:mt-20 mt-10 bg-site-info">
+            <div className=" gap-18 p-4 flex  items-center justify-between">
                 <div className="flex gap-8">
                     {siteInfo.map((info, index) => (
                         <InfoCard key={index} title={info.title} icon={info.icon}/>
                     ))}
                 </div>
                 <div>
-                    <h1 className="text-lg lg:text-2xl font-bold text-secondary">با بیش از ده سال سابقه فروش لوازم ورزشی و لباس های ورزشی</h1>
+                    <h1 className="text-lg w-3/4 lg:text-2xl font-bold text-secondary">با بیش از ده سال سابقه فروش لوازم ورزشی و لباس های ورزشی</h1>
                 </div>
             </div>
         </section>
