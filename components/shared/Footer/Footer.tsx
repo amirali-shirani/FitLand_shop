6bee0bb45ad734a206e7e156d0b85c38ad6bd9d8
Amirali Shirani <amirali7shirani@gmail.com>
2025-09-06 15:36:40 +0330

    added footer contact us

--- components/shared/Footer/Footer.tsx
+++ components/shared/Footer/Footer.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {footerLinks} from "@/lib/data";
 import FooterCol from "@/components/shared/Footer/FooterCol";
 import FooterContactUs from "@/components/shared/Footer/FooterContactUs";
+import FooterAboutUs from "@/components/shared/Footer/FooterAboutUs";
 
 const Footer = () => {
     return (
-        <footer dir="rtl" className="p-8 md:p-12 text-white bg-slate-900">
+        <footer className="p-8 md:p-12 text-white bg-footer-secondary">
             <div className="container mx-auto grid grid-cols-1 lg:grid-cols-6 gap-12 lg:gap-10">
 
                 <div className="lg:col-span-4 grid grid-cols-2 md:grid-cols-4 gap-x-12 gap-y-8">
@@ -22,7 +23,9 @@ const Footer = () => {
                     <FooterContactUs/>
                 </div>
             </div>
-
+            <div>
+                <FooterAboutUs/>
+            </div>
             {/*<div className="container mx-auto mt-12 pt-8 border-t border-gray-700 text-center text-gray-400">*/}
             {/*    <p>&copy; ۲۰۲۵ - تمامی حقوق برای فروشگاه فیت‌لند محفوظ است.</p>*/}
             {/*</div>*/}
---
+++ components/shared/Footer/FooterAboutUs.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import namad1 from "@/public/assets/images/footer/namad/01.svg";
+import namad2 from "@/public/assets/images/footer/namad/02.svg";
+import namad3 from "@/public/assets/images/footer/namad/03 1.svg";
+
+const images = [namad1, namad2, namad3];
+
+const FooterAboutUs = () => {
+    return (
+        <section className="container mx-auto mt-12 grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
+            <article className="space-y-3 leading-6">
+                <h2 className="text-base font-semibold">فروشگاه اینترنتی فیت‌لند</h2>
+                <p className="text-sm  text-justify">
+                    فروشگاه لوازم ورزشی فیت‌لند در سال 1403 کار خود را به صورت حرفه ای آغاز کرد و با هدف ارائه جدیدترین
+                    محصولات ورزشی از قبیل لوازم فوتبال، فوتسال، والیبال، بسکتبال، تنیس و... همچنین پوشاک ورزشی و تجهیزات
+                    سفر، از برند های معتبر دنیا در محیطی کاربرپسند، قابل اطمینان و با مجربترین مشاوران و کارشناسان ورزشی
+                    فعالیت می کند. فروشگاه فیت‌لند دارای نماد اعتماد از وزارت صنعت معدن و تجارت می باشد و تمامی محصولات
+                    خود را با 7 روز ضمانت بازگشت همراه با گارانتی اصالت و سلامت فیزیکی، با سریع ترین روش های ارسال به
+                    سراسر ایران در اختیار مشتریان خود قرار می دهد.
+                </p>
+            </article>
+
+            <div className="flex flex-col items-end space-y-4">
+                <div className="flex gap-4">
+                    {images.map((image, index) => (
+                        <Image
+                            key={index}
+                            alt={`نماد اعتماد ${index + 1}`}
+                            src={image}
+                            width={80}
+                            height={80}
+                            className="object-contain"
+                        />
+                    ))}
+                </div>
+                <h3 className="text-sm font-medium">
+                     یک هفته ضمانت بازگشت ✍
+                </h3>
+            </div>
+        </section>
+    );
+};
+
+export default FooterAboutUs;
